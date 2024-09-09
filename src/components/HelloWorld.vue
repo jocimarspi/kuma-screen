7570e9f1c7c777dd644adac943369016d1b7acad
Jocimar B. A. Huss <jocimarspi@gmail.com>
2024-09-08 21:28:00 -0300

    included lib for parce prometheus format

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue"
+import axios from "axios"
+import parsePrometheusTextFormat from 'parse-prometheus-text-format';
+
+const metrics = ref(null)
+
+onMounted (() => {
+  axios.get("/uptime/metrics", {
+    auth: {
+      username: "",
+      password: "uk1_fRQ9EY9ZJC3RdHhyVxVaR5LAonjz6LJpLPXVXm9o"
+    }
+  }).then(resp => metrics.value = parsePrometheusTextFormat(resp.data))
+  .catch(err => metrics.value = err)
+})
+
+function getMetrics() {
+  if (!metrics.value) return null
+
+  return metrics.value.find((it: any) => it.name === "monitor_status").metrics
+}
+
 defineProps<{
   msg: string
 }>()
+
 </script>
 
 <template>
@@ -12,6 +35,11 @@ defineProps<{
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+    <div>
+      <ul>
+        <li v-for="m in getMetrics()" :key="m.labels.monitor_name">{{m.value +" - "+ m.labels.monitor_name }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      // Quando uma requisição para /api for feita, será redirecionada para http://localhost:3001
+      '/uptime': {
+        target: 'http://localhost:3001', // URL do backend
+        changeOrigin: true,               // Muda a origem para a origem do backend
+        rewrite: (path) => path.replace(/^\/uptime/, ''), // Remove '/api' do caminho
+      },
+    },
+  },
 })
+ 
