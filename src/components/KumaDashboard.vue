b82cda0375f54e7924e0de30f9120487beb56526
Jocimar B. A.  Huss <jocimar.huss@db1.com.br>
2024-09-22 20:24:46 -0300

    fix order and included animations

--- src/components/KumaDashboard.vue
+++ src/components/KumaDashboard.vue
@@ -1,132 +1,150 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted, watch, computed } from 'vue'
+import MonitorCard from './MonitorCard.vue'
+import kumaService, { MonitorStatusEnum, type MonitorStatus } from '@/services/kuma-service'
 
-  import {ref, onMounted, onUnmounted, watch} from 'vue';
-  import MonitorCard from './MonitorCard.vue';
-  import kumaService, { MonitorStatusEnum, type MonitorStatus } from '@/services/kuma-service';
+const monitorStatuses = ref<MonitorStatus[]>()
+const updateTimer = ref(0)
+const lastUpdate = ref<string>('')
 
-  const monitorStatuses = ref<MonitorStatus[]>()
-  const updateTimer = ref(0)
-  const lastUpdate = ref<string>('')
+const downMonitors = computed(() => {
+  return monitorStatuses.value
+    ? monitorStatuses.value.filter((monitor) => monitor.value === MonitorStatusEnum.DOWN)
+    : undefined
+})
 
-  function formatDate(date: Date): string {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses são indexados a partir de 0
-    const year = date.getFullYear();
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    return `${day}/${month}/${year} • ${hours}:${minutes}:${seconds}`;
+function getStatusOrder(monitorStatus: MonitorStatusEnum) {
+  return MonitorStatusEnum.UP === monitorStatus ? '999' : monitorStatus
+}
+
+const upMonitors = computed(() => {
+  return monitorStatuses.value
+    ? monitorStatuses.value
+        .filter((monitor) => monitor.value !== MonitorStatusEnum.DOWN)
+        .sort(
+          (a, b) =>
+            getStatusOrder(a.value) < getStatusOrder(b.value)
+              ? -1
+              : getStatusOrder(a.value) > getStatusOrder(b.value)
+                ? 1
+                : a.labels.monitor_name.localeCompare(b.labels.monitor_name) // a.value.labels.monitor_name.localeCompare(b.value.labels.monitor_name)
+        )
+    : undefined
+})
+
+function formatDate(date: Date): string {
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0') // Meses são indexados a partir de 0
+  const year = date.getFullYear()
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+  return `${day}/${month}/${year} • ${hours}:${minutes}:${seconds}`
+}
+
+async function updateMonitors() {
+  monitorStatuses.value = await kumaService.getMonitorStatus()
+}
+
+async function startTimer() {
+  updateTimer.value = setInterval(async () => {
+    updateMonitors()
+  }, 10000) as unknown as number
+}
+
+function stopTimer() {
+  clearInterval(updateTimer.value)
+}
+
+watch(monitorStatuses, (newValue) => {
+  if (newValue) {
+    lastUpdate.value = formatDate(new Date()) // Atualiza com a data e hora atuais
   }
+})
 
-  async function startTimer() {
-    updateTimer.value = setInterval(async () => {
-      monitorStatuses.value =  await kumaService.getMonitorStatus()
-    }, 10000) as unknown as number
-  }
+onMounted(async () => {
+  updateMonitors()
+  startTimer()
+})
 
-  function stopTimer() {
-    clearInterval(updateTimer.value)
-  }
-
-  watch(monitorStatuses, (newValue) => {
-    if (newValue) {
-      lastUpdate.value = formatDate(new Date()); // Atualiza com a data e hora atuais
-    }
-  });
-
-  onMounted(async () => {
-    monitorStatuses.value =  await kumaService.getMonitorStatus()
-    startTimer()
-  })
-
-  console.log(monitorStatuses)
-
-  onUnmounted(() => {
-    stopTimer()
-  })
-
+onUnmounted(() => {
+  stopTimer()
+})
 </script>
 
 <template>
- 
   <header>
     <h1 class="title">APLICAÇÕES FÁBRICA</h1>
-    <p class="time">Last Update: <span>{{ lastUpdate }}</span></p>
+    <p class="time">
+      Last Update: <span>{{ lastUpdate }}</span>
+    </p>
   </header>
 
-  <div id="down-services">
-    <h2 class="container__title">DOWN ({{ monitorStatuses ? monitorStatuses.filter(monitor => monitor.value === MonitorStatusEnum.DOWN).length : 0 }})</h2>
+  <div v-if="downMonitors?.length > 0" id="down-services">
+    <h2 class="container__title">DOWN ({{ downMonitors?.length }})</h2>
     <div class="container">
-
-      <MonitorCard 
-        v-for="monitorStatus in monitorStatuses?.filter(monitor => monitor.value === MonitorStatusEnum.DOWN)" 
-        :title="monitorStatus.labels.monitor_name" 
-        :key="monitorStatus.labels.monitor_name"
-        :status="monitorStatus.value"
-      />
-      
-    </div>
-  </div>
-
-  <div id="up-services">
-    <h2 class="container__title">UP ({{ monitorStatuses ? monitorStatuses.filter(monitor => monitor.value !== MonitorStatusEnum.DOWN).length : 0 }})</h2>
-    <div class="container">
-
-      <MonitorCard 
-        v-for="monitorStatus in monitorStatuses?.filter(monitor => monitor.value !== MonitorStatusEnum.DOWN)" 
-        :title="monitorStatus.labels.monitor_name" 
+      <MonitorCard
+        v-for="monitorStatus in downMonitors"
+        :title="monitorStatus.labels.monitor_name"
         :key="monitorStatus.labels.monitor_name"
         :status="monitorStatus.value"
       />
     </div>
   </div>
 
+  <div v-if="upMonitors?.length > 0" id="up-services">
+    <h2 class="container__title">UP ({{ upMonitors?.length }})</h2>
+    <div class="container">
+      <MonitorCard
+        v-for="monitorStatus in upMonitors"
+        :title="monitorStatus.labels.monitor_name"
+        :key="monitorStatus.labels.monitor_name"
+        :status="monitorStatus.value"
+      />
+    </div>
+  </div>
 </template>
 
-
-
 <style scoped>
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 60px;
+}
 
-  header {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 60px;
-  }
+h1 {
+  font-size: 4rem;
+  font-weight: 700;
+  color: #acacac;
+}
 
-  h1 {
-    font-size: 4rem;
-    font-weight: 700;
-    color: #acacac;
-  }
+h2 {
+  font-size: 2rem;
+  font-weight: 700;
+  color: #acacac;
+}
 
-  h2 {
-    font-size: 2rem;
-    font-weight: 700;
-    color: #acacac;
-  }
+.container__title {
+  margin-top: 45px;
+  margin-bottom: 15px;
+}
 
-  .container__title {
-    margin-top: 45px;
-    margin-bottom: 15px;
-  }
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
+}
 
-  .container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 15px;
-  }
+.time {
+  font-size: 2rem;
+  font-weight: 300;
+  color: #acacac;
+}
 
-  .time {
-    font-size: 2rem;
-    font-weight: 300;
-    color: #acacac;
-  }
-
-  .time span {
-    font-weight: 700;
-    color: #ffffff;
-  }
-
+.time span {
+  font-weight: 700;
+  color: #ffffff;
+}
 </style>
--- src/components/MonitorCard.vue
+++ src/components/MonitorCard.vue
@@ -1,70 +1,76 @@
 <script setup lang="ts">
+import { defineProps } from 'vue'
+import { MonitorStatusEnum } from '@/services/kuma-service'
 
-  import { defineProps } from 'vue';
+const props = defineProps<{
+  title: string
+  status?: MonitorStatusEnum
+}>()
 
-  const props = defineProps<{
-    title: string;
-    status?: string;
-  }>();
-
-  function getStatusClass() {
-    
-    console.log(props)
-
-    switch (props.status) {
-      case '0': return 'monitor--down';
-      case '1': return 'monitor--up'
-      case '2': return 'monitor--pending';
-      case '3': return 'monitor--maintenance';
-      default: return 'monitor-undefined';
-    }
+function getStatusClass() {
+  switch (props.status) {
+    case MonitorStatusEnum.DOWN:
+      return 'monitor--down'
+    case MonitorStatusEnum.UP:
+      return 'monitor--up'
+    case MonitorStatusEnum.PENDING:
+      return 'monitor--pending'
+    case MonitorStatusEnum.MAINTENANCE:
+      return 'monitor--maintenance'
+    default:
+      return 'monitor-undefined'
   }
-
+}
 </script>
 
 <template>
-
   <div :class="['monitor', getStatusClass()]">
     <h4>{{ title }}</h4>
   </div>
-
 </template>
 
 <style>
-
 .monitor {
-    display: flex;
-    align-items: center;
-    justify-content: start;
-    padding: 30px;
-    font-size: 3rem;
-    font-weight: 700;
-    line-height: 1;
-    text-align: center;
-    border-radius: 0.5vw;
-    width: 100%;
-    background-color: #ffffff;
-    white-space: nowrap;
-    overflow: hidden;
-  }
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding: 30px;
+  font-size: 3rem;
+  font-weight: 700;
+  line-height: 1;
+  text-align: center;
+  border-radius: 0.5vw;
+  width: 100%;
+  background-color: #ffffff;
+  white-space: nowrap;
+  overflow: hidden;
+}
 
-  .monitor--up {
-    background-color: #1a9b27;
-  }
+.monitor--up {
+  background-color: #1a9b27;
+}
 
-  .monitor--down {
-    background-color: #c51515;
-    animation: pulse 1s infinite;
-  }
+.monitor--down {
+  background-color: #c51515;
+  animation: pulse 0.5s ease-in-out infinite alternate;
+}
 
-  .monitor--pending {
-    background-color: #e7c52d;
-    animation: pulse 1s infinite;
-  }
+.monitor--pending {
+  background-color: #e7c52d;
+  animation: pulse 0.5s ease-in-out infinite alternate;
+}
 
-  .monitor--maintenance {
-    background-color: #2998e2;
-    animation: pulse 1s infinite;
-  }
+.monitor--maintenance {
+  background-color: #2998e2;
+  animation: pulse 0.5s ease-in-out infinite alternate;
+}
 
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0.97);
+  }
+}
 </style>
